3316bba31e4e4d67081f64f9c57ab9291f8ddd95
poorwym <wuy691128@gmail.com>
2025-02-28 19:22:59 +0800

    更改前

---
+++ note.md
@@ -0,0 +1,18 @@
+
+Code Snippets：https://gist.github.com/adrianhajdin/b1d33c262941a7e21aad833a1cfc84b1
+Assets, Components, Public folders：https://drive.google.com/drive/folders/1KVU8iaH0E_JFtShNiR3BgCSA3pawXY4Z?usp=share_link
+项目地址
+https://github.com/adrianhajdin/project_3D_developer_portfolio
+
+```bash
+npm create vite@latest . -- --template react
+```
+
+```bash
+npm install tailwindcss @tailwindcss/vite
+```
+
+```bash
+npm install --legacy-peer-deps @react-three/drei @react-three/fiber maath react-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
+```
+
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { About, Contact, Experience, Feedbacks, Hero, Tech, Navbar, Works, StarsCanvas} from './components';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'> {/* bg-primary is a class that sets the background color to the primary color defined in the styles.css file */}
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            
+          </div>
+        </div>
+      </BrowserRouter>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </>
+  )
+}
+
+export default App
